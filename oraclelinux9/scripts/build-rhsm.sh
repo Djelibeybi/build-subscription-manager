2902122bc327065cbc42df85543b092341301d24
Avi Miller <me@dje.li>
2023-03-10 10:57:17 +1100

    fix: check out tag before starting package build
    Signed-off-by: Avi Miller <me@dje.li>

--- oraclelinux9/scripts/build-rhsm.sh
+++ oraclelinux9/scripts/build-rhsm.sh
@@ -3,29 +3,26 @@
 # Copyright (c) 2021, 2023 Avi Miller
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-
 # Clone the repo
 cd /root || exit
 git clone https://github.com/candlepin/subscription-manager.git
 
-# Use tito to build the source RPM
+# Checkout the release tag
 cd /root/subscription-manager || exit
+git checkout "subscription-manager-$RHSM_VERSION-$RHSM_RELEASE"
+
+# Use tito to build the source RPM
 tito build --tag="subscription-manager-$RHSM_VERSION-$RHSM_RELEASE" --srpm --dist=".$RHSM_DIST" --offline
 cp "/tmp/tito/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm" /root/rpmbuild/SRPMS/
 
-# use dnf builddep to install the dependencies for the build
-dnf builddep -y "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
-# use rpmbuild --rebuild to build the binary RPM from the src.rpm created by tito
-rpmbuild --rebuild "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
-
-
-# Import the provided GPG signature and sign the binary RPMs using rpmsign
+# Use rpmbuild to build and sign the binary RPMs
 if [ -f /gpg/key.asc ] && [ -f /gpg/passphrase ] && [ "$GPG_NAME_EMAIL" ]; then
 
   # Import and trust the GPG key
   gpg --import --pinentry-mode loopback --passphrase-file /gpg/passphrase < /gpg/key.asc
   (echo 5; echo y; echo save) | gpg --command-fd 0 --no-tty --no-greeting -q --edit-key "$(gpg --list-packets < /gpg/key.asc | awk '$1=="keyid:"{print$2;exit}')" trust
 
+   SIGN="--sign"
   cd /root/rpmbuild || exit
   cat << EOF >> /root/.rpmmacros
 
@@ -33,14 +30,16 @@ if [ -f /gpg/key.asc ] && [ -f /gpg/passphrase ] && [ "$GPG_NAME_EMAIL" ]; then
 %_gpg_name ${GPG_NAME_EMAIL}
 EOF
 
-  echo "Signing binary RPMs"
-  find /root/rpmbuild/RPMS -name "*.rpm*" -exec rpmsign --addsign --key-id="$GPG_NAME_EMAIL" {} \;
-
 else
   echo "Not signing the packages. One or more of the key.asc and passphrase files and the GPG_NAME_EMAIL environment variable are missing."
 fi
 
+dnf builddep -y "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
+# shellcheck disable=SC2086
+rpmbuild --rebuild $SIGN "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
 
 # Copy the RPMs to the output location
-mkdir /output/oraclelinux9
-cp -r /root/rpmbuild/RPMS/* /output/oraclelinux9/
+if [ ! -d /output/oraclelinux9 ]; then
+  mkdir /output/oraclelinux9
+fi
+cp -rf /root/rpmbuild/RPMS/* /output/oraclelinux9/
