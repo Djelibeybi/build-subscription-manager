52b20b6d247c47456c2f90a5d1cb505b47fe59ed
Avi Miller <me@dje.li>
2024-01-31 10:11:21 +1100

    fix: update Oracle Linux 7 build scripts
    Signed-off-by: Avi Miller <me@dje.li>

--- oraclelinux7/scripts/build-rhsm.sh
+++ oraclelinux7/scripts/build-rhsm.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 #
-# Copyright (c) 2021 Avi Miller
+# Copyright (c) 2021, 2024 Avi Miller
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-# Import GPG key and trust it
-gpg --import --passphrase-file /gpg/passphrase < /gpg/key.asc
-(echo 5; echo y; echo save) | gpg --command-fd 0 --no-tty --no-greeting -q --edit-key "$(gpg --list-packets < /gpg/key.asc | awk '$1=="keyid:"{print$2;exit}')" trust
 
 # Clone the git repo
 cd /root || exit
 git clone https://github.com/candlepin/subscription-manager.git
 
-# Build the SRPM using tito then install it
-cd subscription-manager || exit
+# Checkout the release tag
+cd /root/subscription-manager || exit
+git checkout "subscription-manager-$RHSM_VERSION-$RHSM_RELEASE"
+
+# Use tito to build the source RPM
 tito build --tag="subscription-manager-$RHSM_VERSION-$RHSM_RELEASE" --srpm --dist=".$RHSM_DIST" --offline
 cp "/tmp/tito/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm" /root/rpmbuild/SRPMS/
-rpm -ivh "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
+
 
 # Patch the subscription-manager.spec file
+rpm -ivh "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
 cd /root/rpmbuild/SPECS || exit
 patch -p0 < /obsolete-rhn-rpms.diff
 
@@ -28,6 +29,11 @@ rpmbuild -ba SPECS/subscription-manager.spec
 
 # Sign the binary RPMs if the required files and envvar are provided.
 if [ -f /gpg/key.asc ] && [ -f /gpg/passphrase ] && [ "$GPG_NAME_EMAIL" ]; then
+
+  # Import GPG key and trust it
+  gpg --import --passphrase-file /gpg/passphrase < /gpg/key.asc
+  (echo 5; echo y; echo save) | gpg --command-fd 0 --no-tty --no-greeting -q --edit-key "$(gpg --list-packets < /gpg/key.asc | awk '$1=="keyid:"{print$2;exit}')" trust
+
   echo "%_gpg_name $GPG_NAME_EMAIL" >> /root/.rpmmacros
   find /root/rpmbuild/RPMS -name '*.rpm' -exec /rpm-sign.exp {} \;
 fi
