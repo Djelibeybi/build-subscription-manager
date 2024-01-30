02c1b939e175891b402e7e669666e2ae63a4934c
Avi Miller <me@dje.li>
2024-01-31 10:11:21 +1100

    fix: update Oracle Linux 7 build scripts
    Signed-off-by: Avi Miller <me@dje.li>

--- oraclelinux7/build-rhsm-ol7.sh
+++ oraclelinux7/build-rhsm-ol7.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 #
-# Copyright (c) 2021 Avi Miller
+# Copyright (c) 2021, 2024 Avi Miller
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
 # find the latest upstream version of the subscription-manager RPM
-RHSM_NVR=$(docker run --rm -it -v $PWD/scripts:/scripts registry.access.redhat.com/ubi7/ubi rpm -q --queryformat="%{VERSION}:%{RELEASE}" subscription-manager)
+RHSM_NVR=$(docker run --rm -it -v "$PWD/scripts:/scripts registry.access.redhat.com/ubi7/ubi rpm" -q --queryformat="%{VERSION}:%{RELEASE}" subscription-manager)
 RHSM_VERSION=$(echo "$RHSM_NVR" | cut -d: -f1)
 RHSM_REL=$(echo "$RHSM_NVR" | cut -d: -f2)
 RHSM_RELEASE=$(echo "$RHSM_REL" | cut -d. -f1)
 RHSM_DIST=$(echo "$RHSM_REL" | cut -d. -f2)
+IMG_VER=$(git rev-parse --short=12 HEAD)
 
 # build image if necessary
 {
-  docker image inspect build-rhsm:ol7 &>/dev/null
+  docker image inspect "build-rhsm:ol7-$IMG_VER" &>/dev/null
 } || {
-  docker build -t build-rhsm:ol7 .
+  docker build -t "build-rhsm:ol7-$IMG_VER" .
 }
 
 # build the packages in a container which will delete itself once it's done
-docker run --rm -it --name build-rhsm7 \
+docker run --rm -it \
+    --name build-rhsm-ol7 \
     -v "$PWD/gpg:/gpg" \
     -v "$PWD/output:/output" \
     -e RHSM_VERSION="$RHSM_VERSION" \
     -e RHSM_RELEASE="$RHSM_RELEASE" \
     -e RHSM_DIST="$RHSM_DIST" \
     -e GPG_NAME_EMAIL \
-    build-rhsm:ol7
+    "build-rhsm:ol7-$IMG_VER"
