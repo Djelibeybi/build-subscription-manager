869d82348945c6d580c7eeceadaf73f194b30ccf
Avi Miller <me@dje.li>
2024-01-31 11:32:58 +1100

    fix: simplify RPM signing process for OL8 and OL9
    Signed-off-by: Avi Miller <me@dje.li>

--- oraclelinux9/scripts/build-rhsm.sh
+++ oraclelinux9/scripts/build-rhsm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2021, 2023 Avi Miller
+# Copyright (c) 2021, 2024 Avi Miller
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 # Clone the repo
@@ -15,28 +15,23 @@ git checkout "subscription-manager-$RHSM_VERSION-$RHSM_RELEASE"
 tito build --tag="subscription-manager-$RHSM_VERSION-$RHSM_RELEASE" --srpm --dist=".$RHSM_DIST" --offline
 cp "/tmp/tito/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm" /root/rpmbuild/SRPMS/
 
-# Use rpmbuild to build and sign the binary RPMs
+# Build the binary RPMs
+dnf builddep -y "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
+rpmbuild --rebuild "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
+
+# Sign the binary RPMs if the required files and envvar are provided
 if [ -f /gpg/key.asc ] && [ -f /gpg/passphrase ] && [ "$GPG_NAME_EMAIL" ]; then
 
   # Import and trust the GPG key
   gpg --import --pinentry-mode loopback --passphrase-file /gpg/passphrase < /gpg/key.asc
   (echo 5; echo y; echo save) | gpg --command-fd 0 --no-tty --no-greeting -q --edit-key "$(gpg --list-packets < /gpg/key.asc | awk '$1=="keyid:"{print$2;exit}')" trust
 
-   SIGN="--sign"
-  cd /root/rpmbuild || exit
-  cat << EOF >> /root/.rpmmacros
+  echo "%_gpg_sign_cmd_extra_args  --batch --pinentry-mode loopback --passphrase-file /gpg/passphrase" >> /root/.rpmmacros
+  echo "%_gpg_name $GPG_NAME_EMAIL" >> /root/.rpmmacros
 
-%_gpg_sign_cmd_extra_args  --batch --pinentry-mode loopback --passphrase-file /gpg/passphrase
-%_gpg_name ${GPG_NAME_EMAIL}
-EOF
+  find /root/rpmbuild/RPMS -name '*.rpm' -exec rpmsign --addsign {} \;
 
-else
-  echo "Not signing the packages. One or more of the key.asc and passphrase files and the GPG_NAME_EMAIL environment variable are missing."
 fi
-
-dnf builddep -y "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
-# shellcheck disable=SC2086
-rpmbuild --rebuild $SIGN "/root/rpmbuild/SRPMS/subscription-manager-$RHSM_VERSION-$RHSM_RELEASE.$RHSM_DIST.src.rpm"
 
 # Copy the RPMs to the output location
 if [ ! -d /output/oraclelinux9 ]; then
